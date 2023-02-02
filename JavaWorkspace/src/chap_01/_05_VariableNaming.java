404eb3187a549dbbf11be4edd9cc4804abe24c66
hayeon <s2hy1028@naver.com>
2023-02-02 14:17:23 +0900

    java_0202

--- JavaWorkspace/src/chap_01/_05_VariableNaming.java
+++ JavaWorkspace/src/chap_01/_05_VariableNaming.java
@@ -20,5 +20,20 @@ public class _05_VariableNaming {
         String flightNo = "KE657"; // 항공 편명
         String _flightNo = "KE657"; // 밑줄 시작
         String flight_no_2 = "KE657"; // 밑줄과 숫자 포함
+
+        int accompany = 2; // 동반 가족 수
+        int lengthOfStay = 5; // 체류 기간
+
+        String item1 = "시계";
+        String item2 = "가방";
+        String item3 = "전자제품";
+
+        // 프로그램의 흐름을 위해 사용되는 경우 등 (크게 이름이 중요하지 않을 때)
+        int i = 0;
+        String s = "";
+        String str = "";
+
+        // 절대 변하지 않는 상수는 대문자로
+        final String CODE = "KR";
     }
 }
